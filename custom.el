7d17fcfe48ebc9fe48ade9f9c3e1b6693fe8e127
Lukas <huberniku@gmail.com>
2022-04-05 15:14:03 +0200

    update custom

--- custom.el
+++ custom.el
@@ -8,6 +8,8 @@
 (add-to-list 'auto-mode-alist '("\\.urdf\\'" . xml-mode)) ;; URDF -- Unified Robot Description Format
 (add-to-list 'auto-mode-alist '("\\.urdf\\'" . xml-mode)) ;; ROS -- configurtation files
 
+(add-to-list 'auto-mode-alist '("\\CMakeLists.txt\\'" . yaml-mode))
+
 ;; Custom hot key
 (defun toggle-comment-on-line ()
   "comment or uncomment current line"
@@ -15,7 +17,8 @@
   (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
 
 ;; (global-set-key (kbd "C-'") 'toggle-comment-on-line)
-(global-set-key (kbd "C-;") 'toggle-comment-on-line)
+(global-set-key (kbd "C-;") 'comment-line)
+;; (global-set-key (kbd "C-j") 'toggle-comment-on-line)
 (global-set-key (kbd "C-j") 'toggle-comment-on-line)
 ;; (global-set-key (kbd "C-j") 'newline)
 ;; Which one is better??? try out and change
@@ -32,8 +35,6 @@
   (setq c-basic-offset 2)
   (c-set-offset 'substatement-open 0))
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
-
-(add-to-list 'auto-mode-alist '("\\CMakeLists.txt\\'" . yaml-mode))
 
 ;; Load faster
 (setq xterm-query-timeout nil)
@@ -61,11 +62,6 @@
 (setq c-default-style "user") ;; DOES NOT WORK
 (setq c-indentation-style "user")
 
-(when (executable-find "ipython")
-  (setq python-shell-interpreter "ipython"))
-
-(setq python-shell-interpreter "ipython"
-	  python-shell-interpreter-args "--simple-prompt -i")
 
 ;; Python Hook
 (add-hook 'python-mode-hook
@@ -73,25 +69,8 @@
                       (setq indent-tabs-mode nil
                             tab-width 2))))
 
-;; (add-hook 'python-mode-hook 'anaconda-mode)
 ;; (package-install 'use-package)
 (require 'use-package)
-
-;; (pyenv-mode)
-(use-package pyvenv
-			 :ensure t
-			 :init
-			 ;; (setenv "WORKON_HOME" "/home/__NAME__/anaconda/envs")
-			 (setenv "WORKON_HOME" "/home/lukas/anaconda/envs")
-			 (pyvenv-mode 1)
-			 (pyvenv-tracking-mode 1))
-
-(use-package conda
-  :ensure t
-  :init
-  (setq conda-anaconda-home (expand-file-name "~/anaconda3"))
-  (setq conda-env-home-directory (expand-file-name "~/anaconda3")))
-
 
 ;; show available keybindings after you start typing
 ;; (require 'which-key)
@@ -109,7 +88,33 @@
 
 ;; Install elpy on command line / linux. Check github/wiki for more info.
 (elpy-enable)
-(setenv "WORKON_HOME" "/home/user/anaconda3/envs/")
+
+
+(use-package pyvenv
+  :ensure t
+  :config
+  (pyvenv-mode t)
+  
+  ;; (pyvenv-tracking-mode 1)
+  
+  ;; Set correct Python interpreter
+  (setq pyvenv-post-activate-hooks
+        (list
+		 (lambda ()
+		   (setq python-shell-interpreter
+				 (concat pyvenv-virtual-env "bin/ipython")))))
+  
+  (setq pyvenv-post-deactivate-hooks
+        (list (lambda ()
+				(setq python-shell-interpreter "ipython"))))
+  )
+
+(when (executable-find "ipython")
+  (setq python-shell-interpreter "ipython"))
+
+(setq python-shell-interpreter "ipython"
+	  python-shell-interpreter-args "--simple-prompt -i")
+
 
 ;; Only use when js / vue etc installed
 ;; ;; M-x package-install lsp-mode
@@ -162,6 +167,15 @@
 
 (setq smerge-command-prefix "\C-ch")
 
+;; Don't break lines in the middle of a word
+;; (add-hook 'tex-mode-hook (visual-line-mode t))
+(add-hook 'plain-text-mode-hook (visual-line-mode t))
+;; Latex/fast_obstxbacle_avoidance/
+(add-hook 'latex-mode-hook #'visual-line-mode)
+;; (add-hook 'LaTeX-mode-hook #'visual-line-mode)
+;; (global-visual-line-mode t)
+
+
 ;; (add-to-list 'package-archives
 ;; '("melpa" . "http://melpa.org/packages/") t)
 ;; Install auto-complete
@@ -174,8 +188,13 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-	(feature-mode auto-complete yaml-mode cython-mode python-mode zop-to-char zenburn-theme which-key volatile-highlights use-package undo-tree super-save smartrep smartparens projectile operate-on-number move-text markdown-mode magit json-mode imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell elpy editorconfig easy-kill dockerfile-mode discover-my-major diminish diff-hl crux conda browse-kill-ring beacon anzu ace-window)))
- '(safe-local-variable-values (quote ((flycheck-disabled-checkers emacs-lisp-checkdoc)))))
+	(csv-mode feature-mode auto-complete yaml-mode python-mode zop-to-char zenburn-theme which-key volatile-highlights use-package undo-tree super-save smartrep smartparens projectile operate-on-number move-text markdown-mode magit json-mode imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell elpy editorconfig easy-kill dockerfile-mode discover-my-major diminish diff-hl crux browse-kill-ring beacon anzu ace-window)))
+ '(safe-local-variable-values
+   (quote
+	((TeX-master . "../main")
+	 (TeX-master . t)
+	 (TeX-master . "main")
+	 (flycheck-disabled-checkers emacs-lisp-checkdoc)))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
